b3796d4b124ec03aaf68a7f43138c9711c1ec282
mallvielfrass <jack@bykollab.com>
2023-12-10 18:32:18 +0300

    fix screen and add tests

--- internal/tgRouter/sensetiveTrigger/trigger.go
+++ internal/tgRouter/sensetiveTrigger/trigger.go
@@ -4,19 +4,22 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"sync"
 )
 
 type group struct {
 	chatID int64
 	words  []string
 	regexp *regexp.Regexp
+	regStr string
 }
-type sensetiveTrigger struct {
+type SensetiveTrigger struct {
 	groups map[int64]*group
+	m      sync.RWMutex
 }
 
-func New() *sensetiveTrigger {
-	return &sensetiveTrigger{
+func New() *SensetiveTrigger {
+	return &SensetiveTrigger{
 		groups: make(map[int64]*group),
 	}
 }
@@ -32,13 +35,13 @@ func join(words []string) string {
 		end := ""
 		w := word
 		if !strings.HasPrefix(w, "*") {
-			start += "\b"
+			start += "\\b"
 		} else {
 			w = strings.TrimPrefix(w, "*")
 		}
 
-		if !strings.HasSuffix(word, "*") {
-			end += "\b"
+		if !strings.HasSuffix(w, "*") {
+			end += "\\b"
 		} else {
 			w = strings.TrimSuffix(w, "*")
 		}
@@ -46,43 +49,89 @@ func join(words []string) string {
 	}
 	return strings.Join(proccedWords, "|")
 }
-func (s *sensetiveTrigger) recompile(chatID int64) error {
+func (s *SensetiveTrigger) recompile(chatID int64) error {
 	val, ok := s.groups[chatID]
 	if !ok {
 		return fmt.Errorf("group not found")
 	}
-
+	//fmc.Printfln("words: %+v", val.words)
 	regxStr := join(val.words)
+	//	fmc.Printfln("regexpStr: %v", []byte(regxStr))
 	exp, err := regexp.Compile(regxStr)
 	if err != nil {
 		return err
 	}
 	val.regexp = exp
+	val.regStr = regxStr
 	return nil
 }
 
-func (s *sensetiveTrigger) AddWords(chatID int64, words ...string) error {
+func (s *SensetiveTrigger) AddWords(chatID int64, words ...string) error {
+	s.m.Lock()
 	_, ok := s.groups[chatID]
 	if !ok {
 		s.groups[chatID] = &group{
 			chatID: chatID,
-			words:  words,
 		}
 	}
 
 	s.groups[chatID].words = append(s.groups[chatID].words, words...)
-	return s.recompile(chatID)
+	//fmt.Printf("words in group: %+v\n", s.groups[chatID].words)
+	s.m.Unlock()
+	err := s.recompile(chatID)
+	if err != nil {
+		return err
+	}
+
+	//	fmc.Printfln("regexp: %s", s.groups[chatID].regexp.String())
+	return err
 }
 
-func (s *sensetiveTrigger) ChatIsSensetive(chatID int64) bool {
+func (s *SensetiveTrigger) ChatIsSensetive(chatID int64) bool {
 	_, ok := s.groups[chatID]
 	return ok
 }
-
-func (s *sensetiveTrigger) MessageContainSensitiveWords(chatID int64, message string) bool {
+func regexpMatch(msg string, regx string) (bool, error) {
+	reg, err := regexp.Compile(regx)
+	if err != nil {
+		fmt.Println(err)
+		return false, err
+	}
+	return reg.MatchString(msg), nil
+}
+func (s *SensetiveTrigger) MessageContainSensitiveWords(chatID int64, message string) (bool, error) {
 	_, ok := s.groups[chatID]
 	if !ok {
-		return true
+		return false, fmt.Errorf("group not found")
 	}
-	return s.groups[chatID].regexp.MatchString(message)
+	rRes := s.groups[chatID].regexp.MatchString(message)
+	//fmt.Printf("SensetiveTrigger: [%v], message: %s, regexp: %s\n", rRes, message, s.groups[chatID].regexp.String())
+	return rRes, nil
+}
+
+// func GetRegexp(chatID int64) (*regexp.Regexp, error)
+func (s *SensetiveTrigger) GetRegexp(chatID int64) (*regexp.Regexp, error) {
+	_, ok := s.groups[chatID]
+	if !ok {
+		return nil, fmt.Errorf("group not found")
+	}
+	return s.groups[chatID].regexp, nil
+}
+
+// func GetRegexpStr(chatID int64) (string, error)
+func (s *SensetiveTrigger) GetRegexpStr(chatID int64) (string, error) {
+	_, ok := s.groups[chatID]
+	if !ok {
+		return "", fmt.Errorf("group not found")
+	}
+	return s.groups[chatID].regStr, nil
+}
+
+// func GetWords(chatID int64) ([]string, error)
+func (s *SensetiveTrigger) GetWords(chatID int64) ([]string, error) {
+	_, ok := s.groups[chatID]
+	if !ok {
+		return nil, fmt.Errorf("group not found")
+	}
+	return s.groups[chatID].words, nil
 }
--- internal/tgRouter/sensetiveTrigger/trigger_test.go
+++ internal/tgRouter/sensetiveTrigger/trigger_test.go
@@ -1,6 +1,9 @@
 package sensetivetrigger
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Test_sensetiveTrigger_recompile(t *testing.T) {
 	type fields struct {
@@ -26,7 +29,7 @@ func Test_sensetiveTrigger_recompile(t *testing.T) {
 			args: args{
 				chatID:  1,
 				words:   []string{"123", "321"},
-				regxStr: "\b123\b|\b321\b",
+				regxStr: "\\b123\\b|\\b321\\b",
 			},
 			wantErr: false,
 		},
@@ -39,7 +42,7 @@ func Test_sensetiveTrigger_recompile(t *testing.T) {
 			args: args{
 				chatID:  1,
 				words:   []string{"123"},
-				regxStr: "\b123\b",
+				regxStr: "\\b123\\b",
 			},
 			wantErr: false,
 		},
@@ -70,10 +73,23 @@ func Test_sensetiveTrigger_recompile(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		//toster toster
+		{
+			name: "toster toster",
+			fields: fields{
+				groups: make(map[int64]*group),
+			},
+			args: args{
+				chatID:  1,
+				words:   []string{"toster", "toster"},
+				regxStr: "\\btoster\\b|\\btoster\\b",
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &sensetiveTrigger{
+			s := &SensetiveTrigger{
 				groups: tt.fields.groups,
 			}
 			s.groups[tt.args.chatID] = &group{
@@ -88,6 +104,200 @@ func Test_sensetiveTrigger_recompile(t *testing.T) {
 			//	t.Log(regStr)
 			if regStr != tt.args.regxStr {
 				t.Errorf("sensetiveTrigger.recompile() error = %v, wantErr %v", regStr, tt.args.regxStr)
+			}
+		})
+	}
+}
+
+func TestSensetiveTrigger_MessageContainSensitiveWords(t *testing.T) {
+	type fields struct {
+		groups map[int64]*group
+	}
+	type args struct {
+		chatID    int64
+		message   string
+		words     []string
+		regexpStr string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+
+		{
+			name: "TestSensetiveTrigger_MessageContainSensitiveWords",
+			fields: fields{
+				groups: make(map[int64]*group),
+			},
+			args: args{
+				words:     []string{"toster", "toster"},
+				regexpStr: "\\btoster\\b|\\btoster\\b",
+				chatID:    1,
+				message:   "toster toster",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := New()
+			for _, w := range tt.args.words {
+				err := s.AddWords(tt.args.chatID, w)
+				if err != nil {
+					t.Errorf("SensetiveTrigger.AddWords() error = %v", err)
+					return
+				}
+			}
+			regx, err := s.GetRegexpStr(tt.args.chatID)
+			if err != nil {
+				t.Errorf("SensetiveTrigger.GetRegexp() error = %v", err)
+				return
+			}
+
+			if regx != tt.args.regexpStr {
+				t.Errorf("sensetiveTrigger.recompile() error = %v, wantErr %v", regx, tt.args.regexpStr)
+				return
+			}
+			got, err := s.MessageContainSensitiveWords(tt.args.chatID, tt.args.message)
+			if err != nil {
+				t.Errorf("SensetiveTrigger.MessageContainSensitiveWords() error = %v", err)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("SensetiveTrigger.MessageContainSensitiveWords() = %v, want %v", got, tt.want)
+				return
+			}
+		})
+	}
+}
+
+func TestSensetiveTrigger_AddWords(t *testing.T) {
+	type fields struct {
+		groups map[int64]*group
+	}
+	type args struct {
+		chatID int64
+		words  []string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "TestSensetiveTrigger_AddWords",
+			fields: fields{
+				groups: make(map[int64]*group),
+			},
+			args: args{
+				chatID: 1,
+				words:  []string{"toster", "toster"},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &SensetiveTrigger{
+				groups: tt.fields.groups,
+			}
+			for _, w := range tt.args.words {
+
+				err := s.AddWords(tt.args.chatID, w)
+				if err != nil {
+					t.Errorf("SensetiveTrigger.AddWords() error = %v", err)
+				}
+			}
+			added, err := s.GetWords(tt.args.chatID)
+			if err != nil {
+				t.Errorf("SensetiveTrigger.GetWords() error = %v", err)
+				return
+			}
+			if !reflect.DeepEqual(tt.args.words, added) {
+				t.Errorf("SensetiveTrigger.GetWords() = %v, want %v", added, tt.args.words)
+
+			}
+		})
+	}
+}
+
+func Test_join(t *testing.T) {
+	type args struct {
+		words  []string
+		regStr string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "Test_join",
+			args: args{
+				words:  []string{"toster", "toster"},
+				regStr: "\\btoster\\b|\\btoster\\b",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if join(tt.args.words) != tt.args.regStr {
+				t.Errorf("join() = %v, want %v", tt.args.regStr, tt.args.regStr)
+			}
+		})
+	}
+}
+
+func Test_regexpMatch(t *testing.T) {
+	type args struct {
+		msg  string
+		regx string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{
+			name: "Test_regexpMatch",
+			args: args{
+				msg:  "toster toster",
+				regx: "\\btoster\\b|\\btoster\\b",
+			},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "Test_regexpMatch",
+			args: args{
+				msg:  "tosters toster",
+				regx: "toster",
+			},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "Test_regexpMatch boarder case",
+			args: args{
+				msg:  "tosters toster",
+				regx: "\\btoster\\b",
+			},
+			want:    true,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := regexpMatch(tt.args.msg, tt.args.regx)
+			if err != nil {
+				t.Errorf("regexpMatch() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("regexpMatch() = %v, want %v", got, tt.want)
 			}
 		})
 	}
