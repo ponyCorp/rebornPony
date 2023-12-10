817d5c1f8549467d8b592e098732f7e525727589
mallvielfrass <jack@bykollab.com>
2023-12-10 10:54:08 +0300

    add sender Reply method

--- internal/services/sender/send.go
+++ internal/services/sender/send.go
@@ -17,3 +17,11 @@ func (s *Sender) SendMessage(chatID int64, text string) error {
 	_, err := s.Bot.Send(tgbotapi.NewMessage(chatID, text))
 	return err
 }
+
+// Reply
+func (s *Sender) Reply(chatId int64, msgID int, text string) error {
+	msg := tgbotapi.NewMessage(chatId, text)
+	msg.ReplyToMessageID = msgID
+	_, err := s.Bot.Send(msg)
+	return err
+}
